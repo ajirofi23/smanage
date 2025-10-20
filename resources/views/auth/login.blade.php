1877a53c1dc46884d20046b8beaf1a33e40682b6
TitinAstriy <titin.astriy12@gmail.com>
2025-10-20 20:37:13 +0700

    Fix Login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="utf-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1" />
-  <title>Login - AICC SHE Portal</title>
+  <title>Loginn - AICC SHE Portal</title>
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"/>
 
